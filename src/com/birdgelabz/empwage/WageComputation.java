027549c4cb19a9aec36cef79413b6340958b4bd1
jotirambandgar <bandgar09@gmail.com>
2022-12-22 08:49:20 +0530

    ADDED: code to print welcome msg

---
+++ src/com/birdgelabz/empwage/WageComputation.java
@@ -0,0 +1,7 @@
+package com.birdgelabz.empwage;
+
+public class WageComputation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wage computation program");
+    }
+}
