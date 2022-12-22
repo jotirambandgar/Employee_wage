41e09485debe6bc2abbddf63faa66d46eb31c0b9
jotirambandgar <bandgar09@gmail.com>
2022-12-22 09:23:11 +0530

    ADDED: code to check employee present or not

--- src/com/birdgelabz/empwage/WageComputation.java
+++ src/com/birdgelabz/empwage/WageComputation.java
@@ -2,6 +2,12 @@ package com.birdgelabz.empwage;
 
 public class WageComputation {
     public static void main(String[] args) {
-        System.out.println("Welcome to employee wage computation program");
+        System.out.println("Welcome to Employee wage Computation Program");
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if(empCheck == 1){
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is absent");
+        }
     }
 }
