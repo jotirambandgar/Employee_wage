2742eca2652e0ca2c556b718834f84ca2018dcf6
jotirambandgar <bandgar09@gmail.com>
2022-12-22 09:39:25 +0530

    ADDED: Code to calculate daily wage

--- src/com/birdgelabz/empwage/WageComputation.java
+++ src/com/birdgelabz/empwage/WageComputation.java
@@ -2,12 +2,16 @@ package com.birdgelabz.empwage;
 
 public class WageComputation {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee wage Computation Program");
+        final int WAGE_PER_HR = 20;
+        final int WORKING_HR = 8;
+        int dailyWage = 0;
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if(empCheck == 1){
             System.out.println("Employee is present");
+            dailyWage = WAGE_PER_HR * WORKING_HR;
         } else {
             System.out.println("Employee is absent");
         }
+        System.out.println("Daily wage of employee " + dailyWage);
     }
 }
